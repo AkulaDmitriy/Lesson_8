7987292e3add9343e49be7581520bd3d005256c8
AkulaDmitriy <dmitry.akula1985@gmail.com>
2023-03-26 11:58:57 +0300

    HW 8

---
+++ src/test/java/JUnit5Test.java
@@ -0,0 +1,52 @@
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class JUnit5Test {
+
+    @BeforeAll
+    static void beforeAll() {
+
+        Configuration.browserSize = "1920x1080";
+
+
+    }
+
+
+    @ValueSource(strings = {"standard_user", "performance_glitch_user"})
+    @ParameterizedTest(name = "Проверить что пользователь {0} может залогиниться")
+    public void checkThatUserNameAreValid(String testData) {
+
+        open("https://www.saucedemo.com/");
+        $("#user-name").setValue(testData);
+        $("#password").setValue("secret_sauce");
+        $("#login-button").click();
+        $(".app_logo").shouldHave(Condition.text("Swag Labs"));
+
+    }
+
+
+    @CsvFileSource(resources = "/testdata/checkThatFirstResultContainsExpectedText.csv", delimiter = '|')
+    @ParameterizedTest(name = "При поиске по слову {0}, первый результат содержит текст {1}")
+    public void checkThatFirstResultContainsExpectedText(String testData, String expectedResult) {
+
+        open("https://www.amazon.com/");
+        $("#twotabsearchtextbox").setValue(testData).pressEnter();
+        $x("//div[@data-cel-widget='search_result_1']").shouldHave(Condition.text(expectedResult));
+
+
+    }
+
+}
+
+
---
+++ src/test/java/SelenideLocaleTest.java
@@ -0,0 +1,48 @@
+import com.codeborne.selenide.Configuration;
+import data.Locale;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideLocaleTest {
+
+    @BeforeAll
+    static void beforeAll() {
+
+        Configuration.browserSize = "1920x1080";
+
+    }
+
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                Arguments.of(Locale.Ru, List.of("С чего начать?", "Док", "ЧАВО", "Блог", "Javadoc", "Пользователи", "Отзывы")),
+                Arguments.of(Locale.En, List.of("Quick start", "Docs", "FAQ", "Blog", "Javadoc", "Users", "Quotes"))
+        );
+    }
+
+    @MethodSource("dataProvider")
+    @ParameterizedTest(name = "При выборе {0} языка, пункты основного меню отображаются следующим образом {1}")
+    public void checkThatMainMenuItemsTranslatedIntoChosenLocal(Locale locale, List<String> expectedMenuPoints) {
+
+        open("https://www.selenide.org/");
+        $$("#languages a").find(text(locale.name())).click();
+        $$(".main-menu-pages a").filter(visible).shouldHave(texts(expectedMenuPoints));
+
+
+    }
+
+
+
+}
+
+
